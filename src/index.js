caca4c3de004c05324ac7f7fe6fa8d279b01bbda
Bhupinder Singh Gill <bhupinderhappy777@gmail.com>
2025-09-19 10:33:41 -0700

    Changes to be committed: modified:   package.json modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,38 @@
 // Example Cloudflare Worker for Twilio Token Generation
 // This file should be deployed as a Cloudflare Worker
 
-import Twilio from 'twilio';
+// JWT utilities for Twilio Access Token generation
+function base64url(str) {
+  return btoa(str)
+    .replace(/\+/g, '-')
+    .replace(/\//g, '_')
+    .replace(/=/g, '');
+}
+
+async function signJWT(payload, secret) {
+  const header = {
+    alg: 'HS256',
+    typ: 'JWT'
+  };
+  
+  const encodedHeader = base64url(JSON.stringify(header));
+  const encodedPayload = base64url(JSON.stringify(payload));
+  
+  const data = encodedHeader + '.' + encodedPayload;
+  const encoder = new TextEncoder();
+  const key = await crypto.subtle.importKey(
+    'raw',
+    encoder.encode(secret),
+    { name: 'HMAC', hash: 'SHA-256' },
+    false,
+    ['sign']
+  );
+  
+  const signature = await crypto.subtle.sign('HMAC', key, encoder.encode(data));
+  const encodedSignature = base64url(String.fromCharCode(...new Uint8Array(signature)));
+  
+  return data + '.' + encodedSignature;
+}
 
 export default {
   async fetch(request, env, ctx) {
@@ -17,28 +48,33 @@ export default {
     }
 
     try {
-      // Initialize Twilio client
-      const client = Twilio(env.TWILIO_ACCOUNT_SID, env.TWILIO_AUTH_TOKEN);
+      // Generate Twilio Access Token
+      const now = Math.floor(Date.now() / 1000);
+      const identity = 'user'; // You can make this dynamic based on request
       
-      // Generate access token
-      const AccessToken = Twilio.jwt.AccessToken;
-      const VoiceGrant = AccessToken.VoiceGrant;
+      const payload = {
+        iss: env.TWILIO_API_KEY,
+        sub: env.TWILIO_ACCOUNT_SID,
+        exp: now + 3600, // Token expires in 1 hour
+        iat: now,
+        nbf: now,
+        jti: env.TWILIO_API_KEY + '-' + now,
+        grants: {
+          identity: identity,
+          voice: {
+            outgoing: {
+              application_sid: env.TWILIO_TWIML_APP_SID
+            },
+            incoming: {
+              allow: true
+            }
+          }
+        }
+      };
       
-      const token = new AccessToken(
-        env.TWILIO_ACCOUNT_SID,
-        env.TWILIO_API_KEY,
-        env.TWILIO_API_SECRET,
-        { identity: 'user' }
-      );
+      const token = await signJWT(payload, env.TWILIO_API_SECRET);
       
-      const voiceGrant = new VoiceGrant({
-        outgoingApplicationSid: env.TWILIO_TWIML_APP_SID,
-        incomingAllow: true,
-      });
-      
-      token.addGrant(voiceGrant);
-      
-      return new Response(JSON.stringify({ token: token.toJwt() }), {
+      return new Response(JSON.stringify({ token: token }), {
         headers: {
           'Content-Type': 'application/json',
           'Access-Control-Allow-Origin': '*',
